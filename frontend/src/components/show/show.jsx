3cf104b325c77cf65d81c8eef7330b65a1ef0420
Chen Yin <923684815@qq.com>
2024-11-11 23:14:24 +0800

    frontend-3

--- frontend/src/components/show/show.jsx
+++ frontend/src/components/show/show.jsx
@@ -1,10 +1,49 @@
 import React,{useState,useEffect} from'react';
+import axios from 'axios';
 import './show.css';
 
 const Show = () => {
+    const [data,setData] = useState(
+        {
+            tempureture:0,
+            pressure:0,
+            deepth:0
+        }
+    );
+    const [sign,setSign] = useState(1);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await axios.get('http://localhost:3000/api/data')
+               .then(response => {
+                    setData(response.data);
+                })
+            }catch(error){
+                console.log(error);
+            }
+        };
+
+        //fetchData();
+
+        const {tempureture,pressure,deepth} = data;
+
+        const timer = setTimeout(() => {
+            setSign(!sign);
+        },10000);
+
+        return () => clearTimeout(timer);
+        
+        
+    }, [sign]);
+
     return (
         <div className="show">
-            Show Page
+            <div className='text'>
+                <div className='temp'>temperature:{data.tempureture}</div>
+                <div className='pressure'> pressure:{data.pressure}</div>
+                <div className='deepth'>deepth:{data.deepth}</div>
+            </div>
         </div>
     );
 };
