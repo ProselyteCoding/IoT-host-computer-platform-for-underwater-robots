e4b50a2e9a0d3b86d110ff2a70208d11a85e9b56
Chen Yin <923684815@qq.com>
2024-11-11 14:51:23 +0800

    control-1 finishied

---
+++ frontend/src/components/show/show.jsx
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from'react';
+import './show.css';
+
+const Show = () => {
+    return (
+        <div className="show">
+            Show Page
+        </div>
+    );
+};
+
+export default Show;
